d0220679a4925521be25af806f22c9841e20ae73
perrychen901105 <chenzhipeng901105@gmail.com>
2015-12-01 21:16:11 +0800

    add MAC OS code

---
+++ Begin1/Hello-World/Hello-World/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hello-World
+//
+//  Created by chenzhipeng on 12/1/15.
+//  Copyright © 2015 perry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Begin1/Hello-World/Hello-World/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hello-World
+//
+//  Created by chenzhipeng on 12/1/15.
+//  Copyright © 2015 perry. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
